e90545f3f691cb9b76c84a4d38564fa1b37ed8fa
Khaled <khalegjr@gmail.com>
2022-08-16 17:44:55 -0300

    criando getter para pegar uma nova candle

--- src/store/modules/CandleStore.ts
+++ src/store/modules/CandleStore.ts
@@ -1,3 +1,4 @@
+import Candle from "@/models/Candle";
 import { def } from "@vue/shared";
 import axios from "axios";
 import { Module, VuexModule } from "vuex-module-decorators";
@@ -7,4 +8,17 @@ const http = axios.create({
 });
 
 @Module({ name: "CandleStore" })
-export default class CandleStore extends VuexModule {}
+export default class CandleStore extends VuexModule {
+  private _candles: Candle[] = [];
+
+  get candles() {
+    return this._candles.length > 0
+      ? this._candles.map(c => {
+          return {
+            x: c.finalDateTime.toLocaleTimeString,
+            y: [c.open, c.high, c.low, c.close],
+          };
+        })
+      : [];
+  }
+}
